ddaeb2622eb83ff911204c614464bfc02c2a43a2
nrschlies <nrschlies@gmail.com>
2022-01-06 18:39:44 -0800

    Create led_cam.py

---
+++ led-cam/led_cam.py
@@ -0,0 +1,58 @@
+"""
+By: Noah Schliesman
+Utilizes face_recognition library for RasPi cam and checks if that face recognition is in
+the list of blacklisted users. If the raspberry pi camera detects a blacklisted face,
+a series of Neopixel lights alternate in a blue-red pattern.
+"""
+
+
+import face_recognition
+import picamera
+import numpy as np
+import board
+import neopixel
+import time
+
+#initializes pi camera and neopixels
+cam = picamera.PiCamera()
+cam.resolution = (320, 240)
+output = np.empty((240, 320, 3), dtype=np.uint8)
+pixels = neopixel.NeoPixel(board.D18, 30)
+
+#fill in this list with blacklisted faces
+not_allowed = []
+
+#initialize 2 empty lists for image and encoding face_recognition
+images = []
+blacklisted_encoding = []
+
+#Blacklisting Function sorts through images ad adds them to a processed list
+def blacklisting():
+    for i in range(len(not_allowed)):
+        images.append(face_recognition.load_image_file(not_allowed[i]))
+        blacklisted_encoding.append(face_recognition.face_encodings(images[i]))
+        
+    #validation pattern to capture current faces
+    valid = True
+    while valid:
+        cam.capture(output, format="rgb")
+        locations = face_recognition.face_locations(output)
+        encodings = face_recognition.face_encodings(output, locations)
+        
+    #checks to see if current face is blacklisted 
+    #could implement better sort algorithm in future (w/ better O(n))?
+    for i in encodings:
+        for j in range(len(blacklisted_encoding)):
+            if (face_recognition.compare_faces(blacklisted_encoding[j], i)):
+                return 1
+
+#if blacklisting yields 1 (match), alternating lights shine         
+def main():
+    while (blacklisting() != 1):
+        pixels.fill((255, 0, 0))
+        time.sleep(1)
+        pixels.fill(0, 0, 255)
+        time.sleep(1)
+        
+#call to main     
+main()
