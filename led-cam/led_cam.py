3490d36b560b238f8778ced9443edf25452cc32e
Noah Schliesman <nschliesman@sandiego.edu>
2022-05-10 13:33:25 -0700

    Update led_cam.py
    Cleaned up docstring comments

--- led-cam/led_cam.py
+++ led-cam/led_cam.py
@@ -13,46 +13,45 @@ import board
 import neopixel
 import time
 
-#initializes pi camera and neopixels
+# initializes pi camera and neopixels
 cam = picamera.PiCamera()
 cam.resolution = (320, 240)
 output = np.empty((240, 320, 3), dtype=np.uint8)
 pixels = neopixel.NeoPixel(board.D18, 30)
 
-#fill in this list with blacklisted faces
+# fill in this list with blacklisted faces (images)
 not_allowed = []
 
-#initialize 2 empty lists for image and encoding face_recognition
+# initialize 2 empty lists for image and encoding face_recognition
 images = []
 blacklisted_encoding = []
 
-#Blacklisting Function sorts through images ad adds them to a processed list
 def blacklisting():
+    """
+    Sorts through blacklisted images and adds them to images list.
+    Adds blacklisted face encodings to blacklisted_encoding list.
+    """
     for i in range(len(not_allowed)):
         images.append(face_recognition.load_image_file(not_allowed[i]))
         blacklisted_encoding.append(face_recognition.face_encodings(images[i]))
         
-    #validation pattern to capture current faces
+    # validation pattern to capture current faces
     valid = True
     while valid:
         cam.capture(output, format="rgb")
         locations = face_recognition.face_locations(output)
         encodings = face_recognition.face_encodings(output, locations)
-        
-    #checks to see if current face is blacklisted 
-    #could implement better sort algorithm in future (w/ better O(n))?
-    for i in encodings:
-        for j in range(len(blacklisted_encoding)):
-            if (face_recognition.compare_faces(blacklisted_encoding[j], i)):
-                return 1
-
-#if blacklisting yields 1 (match), alternating lights shine         
-def main():
-    while (blacklisting() != 1):
+  
+        # checks to see if current face is blacklisted 
+        # could implement better sort algorithm in future (w/ better O(n))?
+        for i in encodings:
+            for j in range(len(blacklisted_encoding)):
+                if (face_recognition.compare_faces(blacklisted_encoding[j], i)):
+                    return 1
+               
+if __name__ = "__main__":
+    while (blacklisting() != 1): # if blacklisting yields 1 (match), alternating lights shine 
         pixels.fill((255, 0, 0))
         time.sleep(1)
         pixels.fill(0, 0, 255)
         time.sleep(1)
-        
-#call to main     
-main()
