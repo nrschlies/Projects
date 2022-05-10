9138f685ba2f1d0e2eb242cf24c41e3bebe2b844
Noah Schliesman <nschliesman@sandiego.edu>
2022-05-10 13:27:24 -0700

    Update wiki_unique.py
    Cleaned up docstring comments.

--- wiki-find/wiki_unique.py
+++ wiki-find/wiki_unique.py
@@ -1,9 +1,6 @@
 """
 Written by: Noah Schliesman
-Modified version of wiki_sort to only pick from unusual articles
-TO DO:
-- Differentiate between title link and sub-link
-- Make tkinter box prettier
+Modified version of wiki_sort to only pick from unusual articles.
 """
 
 import wikipedia
@@ -11,16 +8,22 @@ import random
 from tkinter import *
 
 def initialize_wiki(): 
-    #Checks for links in Unusual articles wikipedia page --> list
-    h = wikipedia.page("Wikipedia:Unusual articles") 
-    unusual = h.links
+    """
+    Checks for links in unusual articles wikipedia page. Choses a random 
+    word from those links, and provides a summary of that word.
+    Returns a tuple of the word and the wikipedia summary of that word.
+    """
+    article = wikipedia.page("Wikipedia:Unusual articles") 
+    unusual = article.links
+    word = random.choices(unusual, k=1) 
+    word = "".join(word) #change word to string
+    
+    return (word, wikipedia.summary(word)) 
 
-    word = random.choices(unusual, k=1) #chooses one random word from those links
-    word = "".join(word) #change to string
-    return (word, wikipedia.summary(word)) #summarizes word
-
-#builds tkinter box
 def initialize_box(word, summary):
+    """
+    Creates tkinter window with word and summary.
+    """
     top = Tk()
     text = Text(top)
     text.insert(INSERT, str(summary))
@@ -28,9 +31,6 @@ def initialize_box(word, summary):
     top.title(str(word))
     top.mainloop()
 
-def main():
+if __name__ == '__main__':
     (word, summary) = initialize_wiki()
     initialize_box(word, summary)
-
-if __name__ == '__main__':
-    main()
