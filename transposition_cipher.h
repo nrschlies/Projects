40c8c061ed2982f4c284d2dc59ec64014f55d12f
Noah Schliesman <nschliesman@sandiego.edu>
2023-10-24 16:23:50 -0700

    Delete transposition_cipher.h

--- transposition_cipher.h
+++
@@ -1,7 +0,0 @@
-#ifndef TRANSPOSITION_CIPHER_H
-#define TRANSPOSITION_CIPHER_H
-
-char* transposition_encrypt(const char* plaintext, const char* key);
-char* transposition_decrypt(const char* ciphertext, const char* key);
-
-#endif
