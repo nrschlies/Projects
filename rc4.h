6f8dc273a2145c050c621c6aa3f9005cb146bdf3
Noah Schliesman <nschliesman@sandiego.edu>
2023-10-24 16:23:09 -0700

    Delete rc4.h

--- rc4.h
+++
@@ -1,9 +0,0 @@
-#ifndef RC4_H
-#define RC4_H
-
-#include <stddef.h>
-
-// RC4 encryption/decryption function
-void RC4(unsigned char *key, size_t len_key, unsigned char *plaintext, size_t len_text, unsigned char *ciphertext);
-
-#endif /* RC4_H */
